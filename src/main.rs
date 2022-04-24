14b746707ead6bba53f163dbea3c37aa786942eb
brianjohnpolasek <brianjohnpolasek@gmail.com>
2022-04-24 12:33:08 -0500

    Bot working with +help command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+mod config;
+
+//--- COMMANDS ---//
+const PREFIX: &str = "+";
+
+const HELP_MESSAGE: &str = "Okay Boomer";
+
+const HELP_COMMAND: &str = "help";
+
+//--- FUNCTIONS ---//
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == format!("{}{}", PREFIX, HELP_COMMAND) {
+            if let Err(error) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", error);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    config::config::main();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
